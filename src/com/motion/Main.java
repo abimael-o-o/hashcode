8d71a974225465ca27272ab11b837575bddf3cd8
Ramesh <st3vegaming@gmail.com>
2020-02-05 17:43:38 -0500

    makes a readFile method

--- src/com/motion/Main.java
+++ src/com/motion/Main.java
@@ -1,8 +1,25 @@
 package com.motion;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+    static final  String filePath = "b_small.txt";
 
     public static void main(String[] args) {
-	// write your code here
+	    readFile();
+    }
+
+    public static void readFile(){
+        File file = new File(filePath);
+        try(Scanner input = new Scanner(file)) {
+            if(input.hasNext()){
+                System.out.println(input.next());
+            }
+
+        }catch (FileNotFoundException e){
+            System.out.println(e.fillInStackTrace());
+        }
     }
 }
