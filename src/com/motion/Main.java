a92534419565da8fd288a06816a0e1536e02d4fc
Ramesh <st3vegaming@gmail.com>
2020-02-05 21:28:41 -0500

    builds a algorithms that checks two arrays at a time

--- src/com/motion/Main.java
+++ src/com/motion/Main.java
@@ -2,16 +2,25 @@ package com.motion;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    static final  String filePath = "b_small.txt";
+    //static final  String filePath = "a_example.txt";
+    //static final  String filePath = "b_small.txt";
+    //static final  String filePath = "c_medium.txt";
+    static final  String filePath = "d_quite_big.txt";
+    //static final  String filePath = "e_also_big.txt";
+
     static int maxSlice;
     static int length;
-    static int[] pizzaArray;
+    static List<Integer> pizzaArray = new ArrayList<>();
+
 
     public static void main(String[] args) {
 	    readFile();
+	    result(pizzaArray);
     }
 
     public static void readFile(){
@@ -19,15 +28,43 @@ public class Main {
         try(Scanner input = new Scanner(file)) {
             maxSlice = Integer.parseInt(input.next());
             length = Integer.parseInt(input.next());
-            pizzaArray = new int[length];
             int counter = 0;
             while (input.hasNext()){
-               pizzaArray[counter] = Integer.parseInt(input.next());
+               pizzaArray.add(Integer.parseInt(input.next()));
                counter++;
            }
-            
         }catch (FileNotFoundException e){
             System.out.println(e.fillInStackTrace());
         }
+    }
+
+    public static void result(List<Integer> arr){
+        int max = arr.get(0);
+        int starter = arr.get(0);
+        int lastIndex = 0;
+
+        for (int i = 1; i<arr.size(); i++){
+            int newMax = starter + arr.get(i);
+            if(newMax > max && newMax < maxSlice ){
+                max = newMax;
+                lastIndex = i;
+            }
+            starter = arr.get(i);
+        }
+        pizzaArray.remove(lastIndex);
+        pizzaArray.remove(lastIndex-1);
+
+        for (int i = 0; i < arr.size(); i++){
+            int newMax = max + arr.get(i);
+            if(newMax > max && newMax < maxSlice ){
+                max = newMax;
+            }
+        }
+
+
+        System.out.println("Max: "+maxSlice);
+        System.out.println("Result: "+ max);
+        System.out.println("Difference: "+ (maxSlice-max));
+
     }
 }
