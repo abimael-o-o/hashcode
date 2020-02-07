c51c0a3d53e7c5bc59358607e6523d4469999291
Ramesh <st3vegaming@gmail.com>
2020-02-06 22:36:15 -0500

    does odd and even method 255 difference

--- src/com/motion/Main.java
+++ src/com/motion/Main.java
@@ -8,10 +8,10 @@ public class Main {
 
     // Uncomment the filepaths for each of the tests!
 
-    static final  String filePath = "a_example.txt";
+    //static final  String filePath = "a_example.txt";
     //static final  String filePath = "b_small.txt";
     //static final  String filePath = "c_medium.txt";
-    //static final  String filePath = "d_quite_big.txt";
+    static final  String filePath = "d_quite_big.txt";
     //static final  String filePath = "e_also_big.txt";
 
     static int maxSlice;
@@ -44,11 +44,25 @@ public class Main {
         int max = 0;
         for (int i = arr.size() - 1; i>=0; i--){
             int newMax = arr.get(i) + max;
-            if((newMax > max) && newMax <= maxSlice ){
+          /*  if(newMax%2 == 0 && i-1 < 0 && arr.get(i-1) %2 != 0 && (newMax +arr.get(i-1) <= maxSlice) ){
                 max = newMax;
-                outPutNums.add(i);
             }
+           */
+          if(newMax%2 == 0) {
+              if ((newMax > max) && newMax <= maxSlice) {
+                  max = newMax;
+                  outPutNums.add(i);
+              }
+          }else if(i-1 > 0 && (newMax +arr.get(i-1) <= maxSlice)){
+              max = newMax;
+          }
         }
+        double percentDiff = (1- (double)max / maxSlice) * 100;
+        System.out.println("Max: "+maxSlice);
+        System.out.println("Result: "+ max);
+        System.out.println("Difference: "+ (maxSlice-max));
+        System.out.println("Percent Diff: "+percentDiff+"%");
+
         Collections.reverse(outPutNums);
         System.out.println(outPutNums.size());
         System.out.println(outPutNums);
